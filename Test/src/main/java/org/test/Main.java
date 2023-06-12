0f1ccab192ad94f8db950b7340c5960cb3fed540
Szymon542 <szymon542@gmail.com>
2023-06-12 19:59:37 +0200

    Build: Project

--- Test/src/main/java/org/test/Main.java
+++ Test/src/main/java/org/test/Main.java
@@ -5,7 +5,7 @@ package org.test;
 public class Main {
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
+    
         System.out.printf("Hello and welcome!");
 
         // Press Ctrl+R or click the green arrow button in the gutter to run the code.
